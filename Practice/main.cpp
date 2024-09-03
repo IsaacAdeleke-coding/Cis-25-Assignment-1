23a6c89e7a480167e89b49a4e7e949209ddc02c7
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-09-03 12:45:32 -0700

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Isaac Adeleke on 8/28/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 2;
+    int num2 = 1;
+
+    // Swap the values
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    cout << "Hello\n" << num1 << " " << num2 << endl;
+    return 0;
+}
+
+
